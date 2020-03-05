59a7d8c8f1ea1a2ba0dbdf5c6e329bc80be36d3d
mountaindboz <David.Bosworth@water.ca.gov>
2020-03-05 13:02:56 -0800

    Added code for Figure 8

--- Veg_Senescence/final_report_figures.R
+++ Veg_Senescence/final_report_figures.R
@@ -5,6 +5,7 @@
 # Load packages
 library(tidyverse)
 library(readxl)
+library(lubridate)
 library(openwaterhg)
 
 # Datasets are on SharePoint site for the Open Water Final Report
@@ -19,7 +20,7 @@ sharepoint_path <- normalizePath(
 # Figure 3 ----------------------------------------------------------------
 # 2015 Pilot Study
 # Bar plot of means of 4 groups with error bars indicating their standard deviations
-# Means are of the mass in ng of filtered MeHg in the overlying water after 12 days of incubation
+# Means are the mass in ng of filtered MeHg in the overlying water after 12 days of incubation
 
 # Import Data
 vss_pilot2015_orig <- read_excel(path = paste0(sharepoint_path, "/VegSens_PilotStudies_Data.xlsx"), sheet = "Dec2015")
@@ -63,7 +64,7 @@ vss_pilot2015_text <- vss_pilot2015_clean %>%
   )
 
 # Create figure
-vss_pilot2015_fig <- vss_pilot2015_clean %>% 
+vss_pilot2015_fig3 <- vss_pilot2015_clean %>% 
   ggplot(aes(x = combine_group, y = avg)) +
   geom_col(fill = "#00BE7D") +
   geom_errorbar(
@@ -93,10 +94,80 @@ vss_pilot2015_fig <- vss_pilot2015_clean %>%
 # Export figure
 ggsave(
   "VSS_final_report_fig3.jpg", 
-  plot = vss_pilot2015_fig,
+  plot = vss_pilot2015_fig3,
   dpi = 300,
   width = 4.5, 
   height = 3.5, 
   units = "in"
 )
 
+
+# Figure 8 ----------------------------------------------------------------
+# 2017 Vegetation Senescence Study
+# Bar plot of means of 3 groups with error bars indicating their standard deviations
+# Means are the concentration in ng/L/day of filtered MeHg in the overlying water
+# Facets for each Week of sample collection
+
+# Import Data
+vss_2017_orig <- read_excel(path = paste0(sharepoint_path, "/VegSens_Dec2017_Conc_Data.xlsx"), sheet = "Normal Water Data- R")
+
+# Prepare data for figure
+vss_2017_clean <- vss_2017_orig %>% 
+  # Extract date from dttm variable
+  mutate(SampleDate = as_date(SampleDate)) %>%
+  # Remove Ice Chest Blank and Devegetated Treatments  
+  filter(!str_detect(StationName, "Blank|Devegetated")) %>% 
+  # Only keep MeHg- filtered data and Before Water Change samples
+  filter(Analyte == "MeHg- filtered", SampleTiming == "Before water change") %>% 
+  # Create a new variable Conc, which is a numeric version of Result with the MDL and RL for the ND values
+  add_num_result() %>% 
+  # Divide Conc by 4 to convert to ng/L/day
+  mutate(Conc = signif(Conc/4, 2)) %>% 
+  # Create Treatment and Week variables
+  mutate(
+    Treatment = str_sub(StationName, end = -3),
+    Week = case_when(
+      SampleDate == "2017-12-06" ~ "Week 1",
+      SampleDate == "2017-12-13" ~ "Week 2",
+      SampleDate == "2017-12-20" ~ "Week 3",
+      SampleDate == "2018-01-03" ~ "Week 5"
+    )
+  ) %>% 
+  # Calculate means and standard deviations
+  group_by(Treatment, Week) %>% 
+  summarize(
+    avg = signif(mean(Conc), 2),
+    stdev = signif(sd(Conc), 2)
+  ) %>% 
+  ungroup()
+
+# Create figure
+vss_2017_fig8 <- vss_2017_clean %>% 
+  ggplot(aes(x = Treatment, y = avg, fill = Treatment)) +
+  geom_col() +
+  geom_errorbar(
+    aes(
+      ymin = avg - stdev, 
+      ymax = avg + stdev, 
+      width = 0.25
+    )
+  ) +
+  labs(
+    x = NULL,
+    y = "fMeHg (ng/L/day) +/- SD"
+  ) +
+  facet_wrap(vars(Week)) +
+  add_gen_color_pal(3, "fill") +
+  theme_light() +
+  theme(strip.text = element_text(color = "black")) +
+  guides(fill = "none")
+
+# Export figure
+ggsave(
+  "VSS_final_report_fig8.jpg", 
+  plot = vss_2017_fig8,
+  dpi = 300,
+  width = 5, 
+  height = 4, 
+  units = "in"
+)
