2051308975c8180ade8a044458a115c47b9914e2
mountaindboz <David.Bosworth@water.ca.gov>
2020-04-06 17:49:28 -0700

    Added code for Table B-9

--- YB_Mass_Balance/final_report_tables_tech_appendix.R
+++ YB_Mass_Balance/final_report_tables_tech_appendix.R
@@ -45,5 +45,60 @@ se_water_bal <- se_flows %>%
   conv_fact_samplingevent() %>% 
   arrange(SamplingEvent)
 
+# Clean up
+rm(se_flows, se_flows_bli, se_flows_sum)
+
 # Export Table B-6
 se_water_bal %>% write_excel_csv("table_b-6.csv", na = "N/A")
+
+
+# Table B-9 ---------------------------------------------------------------
+# Total Water Volumes and Water Balances in the Yolo Bypass 
+# For Flood Events in 2014, 2016, and 2017
+
+# Prepare flow data for calculations
+flow_data_clean <- daily_flow_data_all %>% 
+  # Subtract one day from the Outlet and BLI stations to account for 1-day lag in travel time
+  mutate(date_adj = if_else(LocType == "Inlet", Date, Date - 1)) %>% 
+  # Filter time periods for calculations
+  filter(
+    (date_adj >= "2014-12-19" & date_adj <= "2014-12-23") |
+    (date_adj >= "2016-03-14" & date_adj <= "2016-03-19") |
+    (date_adj >= "2017-01-09" & date_adj <= "2017-05-04")
+  )
+
+# Calculate Below Liberty Island flows
+flow_data_clean_bli <- flow_data_clean %>% 
+  filter(LocType == "Below Liberty") %>% 
+  pivot_wider(names_from = StationName, values_from = Flow) %>% 
+  rename(
+    cache = "Cache Slough near Ryer Island",
+    miner = "Miner Slough near Sac River"
+  ) %>% 
+  mutate(Flow = cache - miner) %>% 
+  select(Year, LocType, Flow)
+
+# Calculate total water volumes for each year
+water_vol_total <- flow_data_clean %>% 
+  filter(LocType != "Below Liberty") %>% 
+  bind_rows(flow_data_clean_bli) %>% 
+  group_by(Year, LocType) %>% 
+  summarize(total_vol_maf = signif(sum(Flow)*60*60*24/43560/1e6, 3)) %>% 
+  ungroup()
+
+# Calculate water balances for each year
+water_bal <- water_vol_total %>% 
+  pivot_wider(names_from = LocType, values_from = total_vol_maf) %>% 
+  rename(bli = "Below Liberty") %>% 
+  mutate(
+    per_outlet = round(Outlet/Inlet * 100),
+    per_bli = round(bli/Inlet * 100)
+  )
+
+# Clean up
+rm(flow_data_clean, flow_data_clean_bli, water_vol_total)
+
+# Export Table B-9
+water_bal %>% write_excel_csv("table_b-9.csv", na = "N/A")
+
+
