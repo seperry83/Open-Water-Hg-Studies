7032e40b5e4bc45c9cc8cead0cc8787883041d0b
mountaindboz <David.Bosworth@water.ca.gov>
2020-05-07 17:34:47 -0700

    Updating code with proper rounding procedures

--- YB_Mass_Balance/final_report_tables_tech_appendix.R
+++ YB_Mass_Balance/final_report_tables_tech_appendix.R
@@ -16,6 +16,7 @@ analyte_order <- c(
   "MeHg- filtered",
   "MeHg- particulate",
   "TSS",
+  "VSS",
   "TOC",
   "DOC",
   "POC"
@@ -217,7 +218,7 @@ water_vol_total <- flow_data_clean %>%
   filter(LocType != "Below Liberty") %>% 
   bind_rows(flow_data_clean_bli) %>% 
   group_by(Year, LocType) %>% 
-  summarize(total_vol_maf = signif(sum(Flow)*60*60*24/43560/1e6, 3)) %>% 
+  summarize(total_vol_maf = sum(Flow)*60*60*24/43560/1e6) %>%
   ungroup()
 
 # Calculate water balances for each year
@@ -227,153 +228,122 @@ water_bal <- water_vol_total %>%
   mutate(
     per_outlet = round(Outlet/Inlet * 100),
     per_bli = round(bli/Inlet * 100)
-  )
-
-# Clean up
-rm(flow_data_clean, flow_data_clean_bli, water_vol_total)
+  ) %>% 
+  # Round total volumes to 3 significant figures
+  mutate_at(vars(Inlet, Outlet, bli), signif, digits = 3)
 
 # Export Table B-9
 water_bal %>% write_excel_csv("table_b-9.csv", na = "N/A")
 
+# Clean up
+rm(flow_data_clean, flow_data_clean_bli, water_vol_total, water_bal)
+
 
 # Table B-10 --------------------------------------------------------------
-# Summary statistics for total loads of Hg, MeHg, Organic Carbon and Suspended Solids
-# Totals for inlets, outlet at the Stairsteps, and Below Liberty Island
+# Summary statistics for total inlet loads of Hg, MeHg, Organic Carbon and Suspended Solids
 # For just the sampling events in 2017
 
-# Summarize total load data for Table B-10
-total_loads_summ <- loads_calc %>% 
+# Bring in total load data
+source("YB_Mass_Balance/Loads/Import_Total_Load_Data.R")
+
+# Filter total inlet load data
+loads_inlet_total <- loads_total %>% 
   # Filter load data
   filter(
+    LocType == "Inlet",
     Year == 2017,
-    str_detect(Analyte, "OC$|Hg|TSS"),
-  ) %>% 
-  # Calculate total loads for each sampling event and analyte
-  group_by(SamplingEvent, Analyte, LoadUnits, LocType) %>% 
-  summarize(
-    total_load = sum(Load),
-    min_digits = min(digits)
-  ) %>% 
-  ungroup()
-
-sign_digits <- total_loads_summ %>% 
-  group_by(LocType, Analyte) %>% 
-  summarize(min_digits = min(min_digits))
-
-  # Define analyte order for table
-  mutate(Analyte = factor(Analyte, levels = analyte_order)) %>% 
-  # Calculate summary statistics for each LocType and analyte
-  group_nest(LocType) %>% 
-  mutate(
-    summ_stats = map(
-      data,
-      .f = ~summ_stat(.x, total_load, Analyte, LoadUnits) %>% 
-        mutate_at(vars(Mean:IQR), signif, digits = 3) %>% 
-        select(Analyte, LoadUnits, Minimum, Maximum, Median, Mean, StDev)
-    )
+    str_detect(Analyte, "OC$|Hg|SS$"),
   )
 
-# Export Table B-10
-  # Inlets 
-  total_loads_summ %>% 
-    filter(LocType == "Inlet") %>% 
-    pull(summ_stats) %>% 
-    chuck(1) %>% 
-    write_excel_csv("table_b-10_inlets.csv")
+# Calcuate averages and standard deviations on non-rounded data
+loads_inlet_total_summ1 <- loads_inlet_total %>% 
+  group_by(Analyte, LoadUnits) %>% 
+  summarize(
+    sign_digits = min(digits),
+    Mean = signif(mean(total_load), sign_digits),
+    StDev = signif(sd(total_load), sign_digits)
+  ) %>% 
+  ungroup() %>% 
+  select(-sign_digits)
 
-  # Outlets at Stairsteps
-  total_loads_summ %>% 
-    filter(LocType == "Outlet") %>% 
-    pull(summ_stats) %>% 
-    chuck(1) %>% 
-    write_excel_csv("table_b-10_outlet.csv")
-  
-  # Below Liberty Island
-  total_loads_summ %>% 
-    filter(LocType == "Below Liberty") %>% 
-    pull(summ_stats) %>% 
-    chuck(1) %>% 
-    write_excel_csv("table_b-10_bli.csv")
-  
+# Calculate all other summary statistics on rounded data
+loads_inlet_total_summ2 <- loads_inlet_total %>% 
+  mutate(total_load = signif(total_load, digits)) %>% 
+  summ_stat(total_load, Analyte) %>% 
+  select(-c(Mean, StDev))
+
+# Join Summary Statistics together for Table B-10
+loads_inlet_total_summ <- 
+  left_join(loads_inlet_total_summ1, loads_inlet_total_summ2) %>% 
+  select(Analyte, LoadUnits, Minimum, Maximum, Median, Mean, StDev) %>% 
+  # Define analyte order for table
+  mutate(Analyte = factor(Analyte, levels = analyte_order)) %>%
+  arrange(Analyte)
+
+# Export Table B-10
+loads_inlet_total_summ %>% write_excel_csv("table_b-10.csv")
+
+# Clean up
+rm(
+  loads_total,
+  loads_inlet_total,
+  loads_inlet_total_summ,
+  loads_inlet_total_summ1,
+  loads_inlet_total_summ2
+)
+
 
 # Table B-11 --------------------------------------------------------------
-# Averages and standard deviations for each inlet's load of Hg, MeHg, Organic Carbon and Suspended Solids
+# Averages and standard deviations for each inlet's load of Hg, MeHg, 
+  # Organic Carbon and Suspended Solids
 # For just the sampling events in 2017
 
+# Bring in inlet load data
+source("YB_Mass_Balance/Loads/Import_Inlet_Load_Data.R")
+
 # Filter Inlet Load data
-inlet_loads <- loads_calc %>% 
+loads_inlet_clean <- loads_inlet %>% 
   filter(
     Year == 2017,
-    LocType == "Inlet",
-    str_detect(Analyte, "OC$|Hg|TSS"),
-  ) %>% 
-  # remove a few variables
-  select(-c(Year, Conc:LocType))
-
-# Sum the CCSB Loads
-ccsb_loads <- filter(inlet_loads, str_detect(StationName, "^CCSB"))
-
-ccsb_loads_sum <- ccsb_loads %>%                      
-  group_by(SamplingEvent, Analyte, LoadUnits) %>% 
-  summarize(Load = sum(Load)) %>% 
-  ungroup() %>% 
-  mutate(StationName = "CCSB")
-
-# Add back the summed CCSB loads
-inlet_loads <- inlet_loads %>% 
-  anti_join(ccsb_loads) %>% 
-  bind_rows(ccsb_loads_sum) %>% 
-  # rename some station names
-  mutate(
-    StationName = case_when(
-      StationName == "Knights Landing Ridge Cut" ~ "KLRC",
-      StationName == "Putah Creek at Mace Blvd" ~ "Putah Creek",
-      StationName == "Sac River above the Sacramento Weir" ~ "Sacramento Weir",
-      TRUE ~ StationName
-    )
+    str_detect(Analyte, "OC$|Hg|SS$"),
   ) %>% 
   # apply factor order to Inlets
   conv_fact_inlet_names()
 
-# Clean up
-rm(ccsb_loads, ccsb_loads_sum)
-  
 # Summarize inlet load data for Table B-11
-inlet_loads_summ <- inlet_loads %>% 
-  # Add zero load values to Fremont and Sacramento Weirs when they weren't spilling
-  complete(
-    SamplingEvent, StationName, nesting(Analyte, LoadUnits),
-    fill = list(Load = 0)
-  ) %>% 
+loads_inlet_summ <- loads_inlet_clean %>% 
   # Calculate averages and standard deviations of loads for each inlet
   group_by(StationName, Analyte, LoadUnits) %>% 
   summarize(
-    avg_load = signif(mean(Load), 3),
-    stdev_load = signif(sd(Load), 3)
+    sign_digits = min(digits, na.rm = TRUE),
+    Mean = signif(mean(Load), sign_digits),
+    StDev = signif(sd(Load), sign_digits)
   ) %>% 
-  ungroup()
+  ungroup() %>% 
+  select(-sign_digits)
 
 # Pivot the averages
-inlet_loads_avg <- inlet_loads_summ %>% 
+loads_inlet_avg <- loads_inlet_summ %>% 
   pivot_wider(
-    id_cols = c(Analyte, LoadUnits),
+    id_cols = -StDev,
     names_from = StationName,
-    values_from = avg_load
+    values_from = Mean
   )
 
 # Pivot the standard deviations
-inlet_loads_stdev <- inlet_loads_summ %>% 
+loads_inlet_stdev <- loads_inlet_summ %>% 
   pivot_wider(
-    id_cols = c(Analyte, LoadUnits),
+    id_cols = -Mean,
     names_from = StationName,
-    values_from = stdev_load
-  )
+    values_from = StDev
+  ) %>% 
+  select(-LoadUnits)
 
 # Join averages and standard deviations together into one df
-inlet_loads_summ_c <- 
+loads_inlet_summ_c <- 
   left_join(
-    inlet_loads_avg, inlet_loads_stdev,
-    by = c("Analyte", "LoadUnits"),
+    loads_inlet_avg, loads_inlet_stdev,
+    by = c("Analyte"),
     suffix = c("_avg", "_stdev")
   ) %>% 
   mutate(Analyte = factor(Analyte, levels = analyte_order)) %>% 
@@ -389,42 +359,106 @@ inlet_loads_summ_c <-
   )
 
 # Export Table B-11
-inlet_loads_summ_c %>% write_excel_csv("table_b-11.csv")
+loads_inlet_summ_c %>% write_excel_csv("table_b-11.csv")
+
+# Clean up
+rm(
+  loads_inlet,
+  loads_inlet_avg,
+  loads_inlet_clean,
+  loads_inlet_stdev,
+  loads_inlet_summ,
+  loads_inlet_summ_c,
+  zero_loads
+)
 
 
 # Table B-12 --------------------------------------------------------------
+# Summary statistics for total loads of Hg, MeHg, Organic Carbon and Suspended Solids
+# Totals for the outlet at the Stairsteps and Below Liberty Island
+# For just the sampling events in 2017
+
+# Bring in total load data
+source("YB_Mass_Balance/Loads/Import_Total_Load_Data.R")
+
+# Filter total load data
+loads_total_clean <- loads_total %>% 
+  # Filter load data
+  filter(
+    LocType != "Inlet",
+    Year == 2017,
+    str_detect(Analyte, "OC$|Hg|SS$"),
+  )
+
+# Calcuate averages and standard deviations on non-rounded data
+loads_total_clean_summ1 <- loads_total_clean %>%
+  group_by(LocType, Analyte, LoadUnits) %>% 
+  summarize(
+    sign_digits = min(digits),
+    Mean = signif(mean(total_load), sign_digits),
+    StDev = signif(sd(total_load), sign_digits)
+  ) %>% 
+  ungroup() %>% 
+  select(-sign_digits)
+
+# Calculate all other summary statistics on rounded data
+loads_total_clean_summ2 <- loads_total_clean %>% 
+  mutate(total_load = signif(total_load, digits)) %>%
+  summ_stat(total_load, LocType, Analyte) %>% 
+  select(-c(Mean, StDev))
+
+# Join Summary Statistics together for Table B-12
+loads_total_clean_summ <- 
+  left_join(loads_total_clean_summ1, loads_total_clean_summ2) %>% 
+  select(LocType, Analyte, LoadUnits, Minimum, Maximum, Median, Mean, StDev) %>% 
+  # Define analyte order for table
+  mutate(Analyte = factor(Analyte, levels = analyte_order)) %>%
+  arrange(Analyte) %>% 
+  group_nest(LocType)
+
+# Export Table B-12
+  # Outlet Loads
+  loads_total_clean_summ %>% 
+    filter(LocType == "Outlet") %>% 
+    pull(data) %>% 
+    chuck(1) %>% 
+    write_excel_csv("table_b-12_outlet.csv")
+  
+  # Below Liberty Island Loads
+  loads_total_clean_summ %>% 
+    filter(LocType == "Below Liberty") %>% 
+    pull(data) %>% 
+    chuck(1) %>% 
+    write_excel_csv("table_b-12_bli.csv")
+  
+# Clean up
+rm(
+  loads_total,
+  loads_total_clean,
+  loads_total_clean_summ,
+  loads_total_clean_summ1,
+  loads_total_clean_summ2
+)
+
+
+# Table B-13 --------------------------------------------------------------
 # Averages and standard deviations for net loads of Hg, MeHg, Organic Carbon and Suspended Solids
 # For 3 segments of the Yolo Bypass: upper reach, Liberty Island, and entire Bypass
 # For just the sampling events in 2017
 
-# Calculate net loads for each reach
-net_loads <- loads_calc %>% 
-  # Only include a subset of the data
+# Bring in total load data
+source("YB_Mass_Balance/Loads/Import_Net_Load_Data.R")
+
+# Filter net load data
+loads_net_clean <- loads_net %>% 
+  # Filter load data
   filter(
     Year == 2017,
-    str_detect(Analyte, "OC$|Hg|TSS")
-  ) %>%
-  # Group and sum load data
-  group_by(SamplingEvent, Analyte, LoadUnits, LocType) %>% 
-  summarize(total_load = sum(Load)) %>% 
-  ungroup() %>% 
-  # Calculate net loads for each reach
-  pivot_wider(names_from = LocType, values_from = total_load) %>% 
-  rename(below_liberty = "Below Liberty") %>% 
-  mutate(
-    Upper = Outlet - Inlet,
-    Liberty = below_liberty - Outlet,
-    Entire = below_liberty - Inlet
-  ) %>% 
-  select(-c(Inlet:below_liberty)) %>% 
-  pivot_longer(
-    cols = Upper:Entire,
-    names_to = "Reach",
-    values_to = "net_load"
-  ) %>% 
-  filter(!is.na(net_load))
+    str_detect(Analyte, "OC$|Hg|SS$"),
+  )
 
-# Summarize net load data for each reach for Table B-12
+# START HERE
+# Summarize net load data for each reach for Table B-13
 net_loads_summ <- net_loads %>% 
   # Calculate averages and standard deviations of net loads for each reach
   group_by(Reach, Analyte, LoadUnits) %>% 
@@ -467,6 +501,6 @@ net_loads_summ_c <-
     starts_with("Entire")
   )
 
-# Export Table B-12
-net_loads_summ_c %>% write_excel_csv("table_b-12.csv")
+# Export Table B-13
+net_loads_summ_c %>% write_excel_csv("table_b-13.csv")
 
