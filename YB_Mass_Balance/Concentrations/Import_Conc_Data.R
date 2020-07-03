b5da21d07c7438011038868cbb1f4ae377c22788
mountaindboz <David.Bosworth@water.ca.gov>
2020-07-03 15:28:32 -0700

    Updated script to use the data within the openwaterhg package, not the data stored on SharePoint

--- YB_Mass_Balance/Concentrations/Import_Conc_Data.R
+++ YB_Mass_Balance/Concentrations/Import_Conc_Data.R
@@ -4,27 +4,12 @@
 # Author: Dave Bosworth
 
 # Load packages
-library(readr)
 library(dplyr)
 library(stringr)
+library(openwaterhg)
 
-# Define path on SharePoint site for data
-temp_sharepoint_path <- normalizePath(
-  file.path(
-    Sys.getenv("USERPROFILE"),
-    "California Department of Water Resources/DWR Documents - Open Water Final Report - Documents/Technical Appendices/Technical Appendix-B_Inlet-Outlet/Data/Final"
-  )
-)
-
-# Import Concentration Data
-temp_conc_orig <- 
-  read_csv(
-    paste0(temp_sharepoint_path, "/NormalSamples.csv"),
-    col_types = "????????????c"
-  )
-
-# Clean temp_conc_orig
-temp_conc_clean <- temp_conc_orig %>% 
+# Clean conc_data
+temp_conc_clean <- conc_data %>% 
   # Remove samples with QualCode "R"
   filter(is.na(QualCode) | !str_detect(QualCode, "^R")) %>%
   # Create a new variable Conc, which is a numeric version of Result with the MDL and RL for the ND values
@@ -38,11 +23,8 @@ temp_conc_clean <- temp_conc_orig %>%
   # Clean up df
   select(SampleCode:Analyte, Conc, ResQual:QualCode)
 
-# Import calculated particulate concentration data
-temp_part_conc_orig <- read_csv(paste0(temp_sharepoint_path, "/Particulate_Conc.csv"))
-
-# Bind all concentration data
-all_conc <- bind_rows(temp_conc_clean, temp_part_conc_orig)
+# Bind conc_data with calculated data for the particulate fractions of Hg, MeHg, and organic carbon
+all_conc <- bind_rows(temp_conc_clean, part_conc_calc)
 
 # Clean up
-rm(temp_conc_clean, temp_conc_orig, temp_part_conc_orig, temp_sharepoint_path)
+rm(temp_conc_clean)
